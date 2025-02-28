c2791786acf4488190c709528916530fc4e74059
Christian Kulesza <christian_kulesza@me.com>
2025-02-28 13:17:30 +0100

    Frontend: Remove RouterOutlet

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CalculationFormComponent } from './features/currencyDenomination/calculation-form/calculation-form.component';
 import { DenominationResultTableComponent } from './features/currencyDenomination/denomination-result-table/denomination-result-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    CalculationFormComponent,
-    DenominationResultTableComponent,
-  ],
+  imports: [CalculationFormComponent, DenominationResultTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
